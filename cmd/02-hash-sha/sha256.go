804705185fcde557d80974914951745c52e9b6f3
oliver <oliverustc@gmail.com>
2025-01-03 09:53:07 +0800

    remove useless utils

--- cmd/02-hash-sha/sha256.go
+++ cmd/02-hash-sha/sha256.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"gnarkabc/gnarkwrapper"
 	"gnarkabc/logger"
-	"gnarkabc/utils"
 
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/hash/sha2"
@@ -46,6 +45,22 @@ func (sc *Sha256Circuit) Define(api frontend.API) error {
 	return nil
 }
 
+func (sc *Sha256Circuit) PreCompile(params ...interface{}) {
+	preImageLen := params[0].(int)
+	sc.PreImage = make([]uints.U8, preImageLen)
+}
+
+func (sc *Sha256Circuit) Assign(curveName string, params ...interface{}) {
+	preImage := params[0].(string)
+	preImageBytes := []byte(preImage)
+	preImageU8 := uints.NewU8Array(preImageBytes)
+	hash := sha256.Sum256(preImageBytes)
+	hashU8 := uints.NewU8Array(hash[:])
+	hashU8Arr := [32]uints.U8(hashU8)
+	sc.PreImage = preImageU8
+	sc.Hash = hashU8Arr
+}
+
 func (sc *Sha256Circuit) GenerateValidAssignment(preImage string) {
 	preImageBytes := []byte(preImage)
 	preImageU8 := uints.NewU8Array(preImageBytes)
@@ -56,35 +71,19 @@ func (sc *Sha256Circuit) GenerateValidAssignment(preImage string) {
 	sc.Hash = hashU8Arr
 }
 
-func Sha256Groth16ZK() []utils.ZkDuration {
+func Sha256Groth16ZK() {
 	preImage := "!"
-	var durations []utils.ZkDuration
 	var curveNameList = []string{"BN254", "BLS12-377", "BLS12-381", "BW6-633", "BW6-761", "BLS24-315", "BLS24-317"}
 	for _, curveName := range curveNameList {
 		logger.Info("testing groth16 zk-snark on curve %s", curveName)
 		curve := gnarkwrapper.CurveMap[curveName]
 		var sc Sha256Circuit
-		sc.PreImage = make([]uints.U8, len(preImage))
-		zk := gnarkwrapper.NewGroth16(&sc, curve)
-		zk.Compile()
-		zk.Setup()
-
-		var assign Sha256Circuit
-		assign.GenerateValidAssignment(preImage)
-		zk.Assignment = &assign
-		zk.Prove()
-		zk.Verify()
-		zk.BenchmarkCompile(10)
-		zk.BenchmarkSetup(10)
-		zk.BenchmarkProve(10)
-		zk.BenchmarkVerify(10)
-		durations = append(durations, utils.ZkDuration{
-			CurveName:   curveName,
-			CompileTime: zk.CompileTime.Microseconds(),
-			SetupTime:   zk.SetupTime.Microseconds(),
-			ProveTime:   zk.ProveTime.Microseconds(),
-			VerifyTime:  zk.VerifyTime.Microseconds(),
-		})
+		sc.PreCompile(len(preImage))
+		var scAssign Sha256Circuit
+		scAssign.Assign(curveName, preImage)
+		ProveTime, ConstraintNum, VerifyTime := gnarkwrapper.ZKP("groth16", curve, &sc, &scAssign)
+		logger.Info("ProveTime: %v ms", ProveTime)
+		logger.Info("ConstraintNum: %v", ConstraintNum)
+		logger.Info("VerifyTime: %v ms", VerifyTime)
 	}
-	return durations
 }
